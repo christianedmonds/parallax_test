01d8b853c8e7ffb7438a38a64fb6abcd1fd0112a
Christian Edmonds <christian@ergon.tech>
2017-09-26 08:19:50 -0700

    rid of skrollr

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ function copyShow() {
 }
 
 copyClick.addEventListener('click', copyShow);
+
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
